0a8fbd1327f9d51f78d201ee48d16b0b97e29db2
Derek Huggens <derekhuggens@gmail.com>
2022-02-05 00:09:39 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(ggplot2)
+library(reshape2)
+install.packages("reshape2")
 
 # DELIVERABLE 1
 df <- read.csv(file="MechaCar_mpg.csv", stringsAsFactors=FALSE)
@@ -29,3 +32,32 @@ t.test(lot_2$PSI, mu = 1500)
 #LOT3
 lot_3 = subset(coil_df, Manufacturing_Lot == 'Lot3')
 t.test(lot_3$PSI, mu = 1500)
+
+data("mtcars")
+
+# Visualizations
+
+ggplot(model, aes(x = vehicle_weight, y = mpg, shape = )) + 
+  geom_point() +
+  geom_smooth(method = "lm", col = "red")
+
+
+ggplot(df) + 
+  geom_jitter(aes(vehicle_weight,mpg), colour="purple") + geom_smooth(aes(vehicle_weight,mpg), method=lm, se=FALSE) +
+  geom_jitter(aes(spoiler_angle,mpg), colour="green") + geom_smooth(aes(spoiler_angle,mpg), method=lm, se=FALSE) +
+  geom_jitter(aes(ground_clearance,mpg), colour="red") + geom_smooth(aes(ground_clearance,mpg), method=lm, se=FALSE) +
+  geom_jitter(aes(AWD,mpg), colour="black") + geom_smooth(aes(AWD,mpg), method=lm, se=FALSE) +
+  geom_jitter(aes(vehicle_length,mpg), colour="orange") + geom_smooth(aes(vehicle_length,mpg), method=lm, se=FALSE) +
+  labs(x = "Measurement", y = "MPG)")
+
+
+
+require(ggplot2)
+require(reshape2)
+df_table = df
+setDT(df_table)
+df2 = melt(df_table, id.vars='mpg')
+ggplot(df2) +
+  geom_jitter(aes(value,mpg, colour=variable),) + geom_smooth(aes(value,mpg, colour=variable), method=lm, se=FALSE) +
+  facet_wrap(~variable, scales="free_x") +
+  labs(x = "Measurement", y = "MPG")
